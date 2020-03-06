646dbdcf98f9e97cdc0ebb4add0de93f58024eec
Toby Driscoll <driscoll@udel.edu>
2020-03-06 10:56:52 -0500

    use proper lambda

--- src/scripts/p8.jl
+++ src/scripts/p8.jl
@@ -5,11 +5,11 @@ for N in 6:6:36
     h = 2π/N
     x = [ j*h for j in 1:N ]
     x = L*(x.-π)/π
-    column = vcat( -π^2/(3*h^2)-1/6, [-0.5*(-1)^j/sin(h*j/2)^2 for j in 1:N-1] )
+    column = [ -π^2/(3*h^2)-1/6; [-0.5*(-1)^j/sin(h*j/2)^2 for j in 1:N-1] ]
     D2 = (π/L)^2*toeplitz(column)  # 2nd-order differentiation
-    eigenvalues = eigvals(-D2 + Diagonal(x.^2))
+    λ = eigvals(-D2 + Diagonal(x.^2))
     @show N
-    [ println(eigenvalues[i]) for i=1:4 ]
+    [ println(λ[i]) for i=1:4 ]
     println("")
 end
 plt = nothing
